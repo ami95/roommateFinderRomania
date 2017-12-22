590f61763f0b3771d6a7a276c2656505fc5dc090
ami95 <mihaiadrian1995@gmail.com>
2017-12-22 17:32:37 +0200

    Create view for rendering home page

--- RoommateFinder/urls.py
+++ RoommateFinder/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from accounts import views as accounts_views
 
 urlpatterns = [
+    url(r'^$', accounts_views.index, name='index'),
     url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
